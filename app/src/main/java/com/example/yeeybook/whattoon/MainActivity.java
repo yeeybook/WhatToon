78868c8661dbc681b2e371d604b12ffd146108b5
sujin31 <dltnwls301@gmail.com>
2020-09-23 22:50:04 +0900

    수진 로그인창에서 버튼 이동까지

--- app/src/main/java/com/example/yeeybook/whattoon/MainActivity.java
+++ app/src/main/java/com/example/yeeybook/whattoon/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.yeeybook.whattoon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view)
+    {
+        Intent intent = new Intent(this, activity_login.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/yeeybook/whattoon/activity_login.java
@@ -0,0 +1,42 @@
+package com.example.yeeybook.whattoon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class activity_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        View.OnClickListener listener = new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                switch (v.getId())
+                {
+                    case R.id.button2:
+                        Intent intent = new Intent(activity_login.this, activity_join.class);
+                        startActivity(intent);
+                        break;
+
+                    case R.id.button3:
+                        break;
+                }
+            }
+        };
+
+        //회원가입 버튼
+        Button join_btn = (Button)findViewById(R.id.button2);
+        join_btn.setOnClickListener(listener);
+        //ID/PW찾기 버튼
+        Button find_btn = (Button)findViewById(R.id.button3);
+        find_btn.setOnClickListener(listener);
+
+    }
+
+}
