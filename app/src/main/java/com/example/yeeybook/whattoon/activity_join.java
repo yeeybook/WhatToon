7434e59d9f98ca45298c290ca15ced83264c9a73
sujin31 <dltnwls301@gmail.com>
2020-09-23 23:38:28 +0900

    수진 join 이제 점점 힘들다

---
+++ app/src/main/java/com/example/yeeybook/whattoon/activity_join.java
@@ -0,0 +1,14 @@
+package com.example.yeeybook.whattoon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_join extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+    }
+}
