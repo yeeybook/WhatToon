814d16a93a046e74da04bcb181e583c3997aa1f6
sujin31 <dltnwls301@gmail.com>
2020-10-14 21:47:41 +0900

    회원가입 및 로그인 기능 구현 회원가입 db -email, uid, gender, name 저장
    app/bulid.gradle 수정함

--- app/src/main/java/com/example/yeeybook/whattoon/JoinActivity.java
+++ app/src/main/java/com/example/yeeybook/whattoon/JoinActivity.java
@@ -1,14 +1,99 @@
 package com.example.yeeybook.whattoon;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class JoinActivity extends AppCompatActivity {
+
+    private EditText EmailJoin;
+    private EditText PwdJoin;
+    private Button Btn;
+    private EditText NameJoin;
+    private RadioGroup RadioGroup;
+    FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        EmailJoin = (EditText) findViewById(R.id.EmailTxt);
+        PwdJoin = (EditText) findViewById(R.id.PasswordTxt);
+        NameJoin=(EditText)findViewById(R.id.NameTxt);
+        RadioGroup = (RadioGroup) findViewById(R.id.radioGroupGender);
+        Btn = (Button) findViewById(R.id.JoinBtn);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int id = RadioGroup.getCheckedRadioButtonId();
+                RadioButton rb = (RadioButton) findViewById(id);
+
+                final String gender = rb.getText().toString();
+                final String email = EmailJoin.getText().toString().trim();
+                final String pwd = PwdJoin.getText().toString().trim();
+                final String name = NameJoin.getText().toString().trim();
+                //공백인 부분을 제거하고 보여주는 trim();
+
+
+                firebaseAuth.createUserWithEmailAndPassword(email, pwd)
+                        .addOnCompleteListener(JoinActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (task.isSuccessful()) {
+
+                                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                                    String uid = user.getUid();
+
+                                    //해쉬맵 테이블을 파이어베이스 데이터베이스에 저장
+                                    HashMap<Object,String> hashMap = new HashMap<>();
+
+                                    hashMap.put("email",email);
+                                    hashMap.put("uid",uid);
+                                    hashMap.put("gender",gender);
+                                    hashMap.put("name",name);
+
+                                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                    DatabaseReference reference = database.getReference("Users");
+                                    reference.child(uid).setValue(hashMap);
+
+                                    Intent intent = new Intent(JoinActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                    Toast.makeText(JoinActivity.this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show();
+
+
+                                } else {
+                                    Toast.makeText(JoinActivity.this, "등록 에러", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
--- app/src/main/java/com/example/yeeybook/whattoon/LoginActivity.java
+++ app/src/main/java/com/example/yeeybook/whattoon/LoginActivity.java
@@ -1,61 +1,68 @@
 package com.example.yeeybook.whattoon;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button join;
+    private Button login;
+    private EditText email_login;
+    private EditText pwd_login;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        join = (Button) findViewById(R.id.JoinBtn);
+        login = (Button) findViewById(R.id.LoginBtn);
+        email_login = (EditText) findViewById(R.id.EmailTxt);
+        pwd_login = (EditText) findViewById(R.id.PasswordTxt);
+        firebaseAuth = firebaseAuth.getInstance();//firebaseAuth의 인스턴스를 가져옴
 
-        //버튼 분기
-        View.OnClickListener listener = new View.OnClickListener(){
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                switch (v.getId())
-                {
+            public void onClick(View v) {
+                String email = email_login.getText().toString().trim();
+                String pwd = pwd_login.getText().toString().trim();
+                //String형 변수 email.pwd(edittext에서 받오는 값)으로 로그인하는것
+                firebaseAuth.signInWithEmailAndPassword(email, pwd)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {//성공했을때
+                                    Intent intent = new Intent(LoginActivity.this, BottomActivity.class);
+                                    startActivity(intent);
+                                } else {//실패했을때
+                                    Toast.makeText(LoginActivity.this, "로그인 오류", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
 
-                    //로그인(임시버튼). 홈으로 이동
-                    case R.id.button:
-                        Intent intent=new Intent(LoginActivity.this, BottomActivity.class);
-                        startActivity(intent);
-                        break;
-
-                    // 회원가입 페이지 이동
-                    case R.id.button2:
-                        Intent intent2 = new Intent(LoginActivity.this, JoinActivity.class);
-                        startActivity(intent2);
-                        break;
-
-                    // ID/PW 찾기 페이지 이동
-                    //case R.id.button2:
-                    //    break;
-
-                }
             }
-        };
+        });
 
-
-        //로그인(임시버튼)
-        Button temp_btn = (Button)findViewById(R.id.button);
-        temp_btn.setOnClickListener(listener);
-        //회원가입 버튼
-        Button join_btn = (Button)findViewById(R.id.button2);
-        join_btn.setOnClickListener(listener);
-        //ID/PW찾기 버튼
-        /*
-        Button find_btn = (Button)findViewById(R.id.button2);
-        find_btn.setOnClickListener(listener);
-         */
-
-
+        join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, JoinActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
